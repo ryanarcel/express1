4801ec422409687f6e3db944ddf9813b25e3b875
Ryan Arcel Galendez <37064027+ryanarcel@users.noreply.github.com>
2024-02-02 05:15:44 +0800

    setup middleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,22 +1,26 @@
 const getGoals = (req, res) => {
-    res.status(200).json({message: "Get Goals"});
-}
+  res.status(200).json({ message: "Get Goals" });
+};
 
 const setGoals = (req, res) => {
-    res.status(200).json({message: "Set Goals"});
-}
+  if(!req.body.text) {
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+  res.status(200).json({ message: "Set Goals" });
+};
 
 const updateGoal = (req, res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}`});
-}
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
 
 const deleteGoal = (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`});
-  }
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
 
 module.exports = {
-    getGoals,
-    setGoals,
-    updateGoal,
-    deleteGoal
-}
+  getGoals,
+  setGoals,
+  updateGoal,
+  deleteGoal,
+};
