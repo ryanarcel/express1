609ab565215b80f4ada1dcc471ec9b056a02b526
Ryan Arcel Galendez <ryanargee2@gmail.com>
2024-02-01 15:02:32 +0800

    arranging routes and controllers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,1 +1,22 @@
-const getGoals = (req)
+const getGoals = (req, res) => {
+    res.status(200).json({message: "Get Goals"});
+}
+
+const setGoals = (req, res) => {
+    res.status(200).json({message: "Set Goals"});
+}
+
+const updateGoal = (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`});
+}
+
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`});
+  }
+
+module.exports = {
+    getGoals,
+    setGoals,
+    updateGoal,
+    deleteGoal
+}
