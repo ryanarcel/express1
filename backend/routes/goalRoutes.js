8717b6fa012ac7094b309506a01639adda3f4404
ryanarcel <ryanargee2@gmail.com>
2024-02-18 13:21:47 +0800

    Setup front end routes and login, register pages

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router()
 const { getGoals, setGoals, updateGoal, deleteGoal } = require('../controllers/goalController')
 const { protect } = require('../middleware/authMiddleware')
 
-
 router.get("/", protect, getGoals)
 
 router.post("/", protect, setGoals)
